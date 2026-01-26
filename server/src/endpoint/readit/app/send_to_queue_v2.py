a4702909e3d3be263f5338365f6587efb18d74a9
Jonghyun Park <parjong@gmail.com>
2026-01-26 23:17:21 +0900

    Integrate 'send-to-queue-v2' app (#5)
    Let's introduce a new simplified queue as a step to recording "title".
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

---
+++ server/src/endpoint/readit/app/send_to_queue_v2.py
@@ -0,0 +1,76 @@
+import click
+
+from gql import Client
+from gql import gql
+from gql.transport.requests import RequestsHTTPTransport as HTTPTransport
+
+from logging import getLogger
+import os
+
+from endpoint.readit.core import Page
+from endpoint.readit.core import page_of_
+
+
+logger = getLogger(__name__)
+logger.setLevel(os.environ.get("ENTRYPOINT_LOG_LEVEL", "INFO").upper())
+
+
+class AddProjectV2DraftIssue:
+    QUERY = gql("""
+    mutation ($projectId: ID!, $title: String!, $body: String!) {
+      addProjectV2DraftIssue(input: {
+        projectId: $projectId,
+        title: $title,
+        body: $body,
+      }) { projectItem { id } }
+    }
+    """)
+
+    def __init__(self, *, projectId: str, title: str, body: str):
+        self._values = {
+            "projectId": projectId,
+            "title": title,
+            "body": body,
+        }
+
+    def execute(self, client):
+        result = client.execute(self.QUERY, variable_values=self._values)
+        logger.debug(result)
+        pass
+
+
+class Queue:
+    PROJECT_ID = "PVT_kwHOAOPA3c4BNgtr"
+
+    def __init__(self):
+        github_graphql_url = os.environ["GITHUB_GRAPHQL_URL"]
+
+        owner_token = os.environ["OWNER_TOKEN"]
+
+        self._client = Client(
+            transport=HTTPTransport(
+                url=github_graphql_url,
+                headers={"Authorization": f"Bearer {owner_token}"},
+            )
+        )
+
+    def add(self, page: Page):
+        AddProjectV2DraftIssue(
+            projectId=self.PROJECT_ID, title=page.title, body=page.url_as_str()
+        ).execute(self._client)
+
+
+@click.command()
+@click.argument("user_url")
+def main(user_url: str) -> None:
+    logger.info("Add '%s'", user_url)
+
+    page = page_of_(user_url)
+
+    logger.info("page = '%s'", page)
+
+    queue = Queue()
+
+    queue.add(page)
+
+    logger.info("Done")
