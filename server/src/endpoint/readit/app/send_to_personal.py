b81211012f2135c69432f8a8b45bb64c0c0c54b9
Jonghyun Park <parjong@gmail.com>
2026-01-26 22:33:01 +0900

    Extend readit endpoint
    Let's post a discussion for each URL.
    
    Signed-off-by: Jonghyun Park <parjong@gmail.com>

---
+++ server/src/endpoint/readit/app/send_to_personal.py
@@ -0,0 +1,102 @@
+import arxiv
+
+import click
+
+from gql import Client
+from gql import gql
+from gql.transport.requests import RequestsHTTPTransport as HTTPTransport
+
+from logging import getLogger
+import os
+
+from endpoint.readit.core import Page
+from endpoint.readit.core import page_of_
+
+
+logger = getLogger(__name__)
+logger.setLevel(os.environ.get("ENTRYPOINT_LOG_LEVEL", "INFO").upper())
+
+
+class CreateDiscussion:
+    QUERY = gql("""
+    mutation ($repositoryId: ID!, $categoryId: ID!, $title: String!, $body: String!) {
+      createDiscussion(input: {
+          repositoryId: $repositoryId,
+          categoryId: $categoryId,
+title: $title,
+          body: $body
+      }) { discussion { id } }
+    }
+    """)
+
+    def __init__(self, *, repositoryId: str, categoryId: str, title: str, body: str):
+        self._values = {
+            "repositoryId": repositoryId,
+            "categoryId": categoryId,
+            "title": title,
+            "body": body,
+        }
+
+    def execute(self, client):
+        result = client.execute(self.QUERY, variable_values=self._values)
+        logger.debug(result)
+        pass
+
+
+class PersonalStorage:
+    REPOSITORY_ID = "MDEwOlJlcG9zaXRvcnk4NTc2NDg5Mw=="
+
+    def __init__(self):
+        github_graphql_url = os.environ["GITHUB_GRAPHQL_URL"]
+
+        owner_token = os.environ["OWNER_TOKEN"]
+
+        self._client = Client(
+            transport=HTTPTransport(
+                url=github_graphql_url,
+                headers={"Authorization": f"Bearer {owner_token}"},
+            )
+        )
+
+    def add_arXiv_article(self, arxiv_id: str):
+        search = arxiv.Search(id_list=[arxiv_id])
+        results = list(search.results())
+        paper = results[0]
+
+        lines = [f"https://arxiv.org/abs/{arxiv_id}", "", f"> {paper.summary}"]
+
+        CreateDiscussion(
+            repositoryId=self.REPOSITORY_ID,
+            categoryId="DIC_kwDOBRyrHc4Cz64i",
+            title=f"[{paper.published.year}] {paper.title}",
+            body="\n".join(lines),
+        ).execute(self._client)
+
+    def add_other_article(self, page: Page):
+        CreateDiscussion(
+            repositoryId=self.REPOSITORY_ID,
+            categoryId="DIC_kwDOBRyrHc4Cz61s",
+            title=f"[YYYY/MM/DD] {page.title}",
+            body=f"{page.url_as_str()}",
+        ).execute(self._client)
+
+
+@click.command()
+@click.argument("user_url")
+def main(user_url: str) -> None:
+    logger.info("Add '%s'", user_url)
+
+    page = page_of_(user_url)
+
+    logger.info("page = '%s'", page)
+
+    storage = PersonalStorage()
+
+    if page.url.netloc == "arxiv.org":
+        arxiv_id = page.url.path.split("/")[-1]
+        storage.add_arXiv_article(arxiv_id)
+        # TODO Implement fallback to "other" feature
+    else:
+        storage.add_other_article(page)
+
+    logger.info("Done")
